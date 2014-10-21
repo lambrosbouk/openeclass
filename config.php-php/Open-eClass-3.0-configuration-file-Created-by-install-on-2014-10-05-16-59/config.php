291088b9ec601783f5a6fb95ca45f846ac6d90f1
lambrosbouk <lambrosbouk@outlook.com>
2014-10-21 19:10:30 +0300

    Create config.php

---
+++ config.php-php/Open-eClass-3.0-configuration-file-Created-by-install-on-2014-10-05-16-59/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-05 16:59
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = '';
+$mysqlMainDb = 'eclass';
