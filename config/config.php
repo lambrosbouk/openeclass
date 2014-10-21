2afa1395028b0303077033eeaf4f900b9620f1a7
Lampros Boukouvalas <lambrosbouk@outlook.com>
2014-10-21 21:12:22 +0300

    Add to hackerearth

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-05 16:59
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = '';
+$mysqlMainDb = 'eclass';
+//hackerearth key:
+$hackerEarthKey = 'ef34f0279e1af1fd967cbf7bd6562bece9e05041';
